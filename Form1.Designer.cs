68cf696d175da6eeb09f9a3324e0bae43bb1de3c
JoshuaMaitland <joshuamaitland1999@outlook.com>
2025-08-03 11:16:15 +0100

    Form1 Preparation
    - Added new labels (`lblListenersCount`, `lblListenersPeak`) to display listener stats.
    - Adjusted Form1 layout and properties (e.g., `ClientSize`, `Text`, `MaximizeBox`).

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblListenersCount = new System.Windows.Forms.Label();
+            this.lblListenersPeak = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblListenersCount
+            // 
+            this.lblListenersCount.AutoSize = true;
+            this.lblListenersCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListenersCount.Location = new System.Drawing.Point(12, 9);
+            this.lblListenersCount.Name = "lblListenersCount";
+            this.lblListenersCount.Size = new System.Drawing.Size(371, 51);
+            this.lblListenersCount.TabIndex = 0;
+            this.lblListenersCount.Text = "Listeners Count: 0";
+            // 
+            // lblListenersPeak
+            // 
+            this.lblListenersPeak.AutoSize = true;
+            this.lblListenersPeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListenersPeak.Location = new System.Drawing.Point(12, 70);
+            this.lblListenersPeak.Name = "lblListenersPeak";
+            this.lblListenersPeak.Size = new System.Drawing.Size(355, 51);
+            this.lblListenersPeak.TabIndex = 1;
+            this.lblListenersPeak.Text = "Listeners Peak: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 161);
+            this.Controls.Add(this.lblListenersPeak);
+            this.Controls.Add(this.lblListenersCount);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(450, 200);
+            this.Name = "Form1";
+            this.Text = "Tarka Radio Listeners Stats";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblListenersCount;
+        private System.Windows.Forms.Label lblListenersPeak;
     }
 }
 
