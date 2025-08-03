b72e03c45c2cceaa95ce14631dab37436211740d
JoshuaMaitland <joshuamaitland1999@outlook.com>
2025-08-03 21:16:26 +0100

    Adjusted the frmMain minimum size to 475x200 to ensure that the git commit in the title can read properly.

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -75,7 +75,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(434, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(459, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -125,7 +125,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 161);
+            this.ClientSize = new System.Drawing.Size(459, 161);
             this.Controls.Add(this.linkLabelWebsite);
             this.Controls.Add(this.lblListenersPeak);
             this.Controls.Add(this.lblListenersCount);
@@ -133,7 +133,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(450, 200);
+            this.MinimumSize = new System.Drawing.Size(475, 200);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tarka Radio Listeners Stats";
