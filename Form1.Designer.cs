f8ea85fc8576d2fd63ff0906b1b4554e664d2a3d
JoshuaMaitland <joshuamaitland1999@outlook.com>
2025-08-03 11:52:20 +0100

    Added Help menu strip
    I also had to move the labels to make the Help menu accessible.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,17 @@
             this.lblListenersCount = new System.Windows.Forms.Label();
             this.lblListenersPeak = new System.Windows.Forms.Label();
             this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblListenersCount
             // 
             this.lblListenersCount.AutoSize = true;
             this.lblListenersCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListenersCount.Location = new System.Drawing.Point(12, 9);
+            this.lblListenersCount.Location = new System.Drawing.Point(12, 25);
             this.lblListenersCount.Name = "lblListenersCount";
             this.lblListenersCount.Size = new System.Drawing.Size(371, 51);
             this.lblListenersCount.TabIndex = 0;
@@ -49,7 +53,7 @@
             // 
             this.lblListenersPeak.AutoSize = true;
             this.lblListenersPeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblListenersPeak.Location = new System.Drawing.Point(12, 70);
+            this.lblListenersPeak.Location = new System.Drawing.Point(12, 80);
             this.lblListenersPeak.Name = "lblListenersPeak";
             this.lblListenersPeak.Size = new System.Drawing.Size(355, 51);
             this.lblListenersPeak.TabIndex = 1;
@@ -60,6 +64,30 @@
             this.timerRefresh.Enabled = true;
             this.timerRefresh.Interval = 10000;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(434, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -67,12 +95,16 @@
             this.ClientSize = new System.Drawing.Size(434, 161);
             this.Controls.Add(this.lblListenersPeak);
             this.Controls.Add(this.lblListenersCount);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(450, 200);
             this.Name = "Form1";
             this.Text = "Tarka Radio Listeners Stats";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,6 +115,9 @@
         private System.Windows.Forms.Label lblListenersCount;
         private System.Windows.Forms.Label lblListenersPeak;
         private System.Windows.Forms.Timer timerRefresh;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
